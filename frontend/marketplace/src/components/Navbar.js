15cfb0edd8079f1c6a4f3c0ee8ed9474e5feb655
surya2003-real <sgoel1811@gmail.com>
2023-09-16 23:18:30 +0530

    bug fixed in hosting the site

--- frontend/marketplace/src/components/Navbar.js
+++ frontend/marketplace/src/components/Navbar.js
@@ -2,14 +2,16 @@
 
 import React from 'react';
 import './Navbar.css'; // Import the CSS file
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Navbar = () => {
   return (
     <nav className="Navbar">
       <div className="Navbar-links">
-        <a href="/">Home</a>
-        <a href="/cart">Cart</a>
-        <a href="/registerproduct">Register Product</a>
+        <LinkContainer to="/"><Nav.Link className="navbar-link">Home</Nav.Link></LinkContainer>
+        <LinkContainer to="/cart"><Nav.Link className="navbar-link">Cart</Nav.Link></LinkContainer>
+        <LinkContainer to="/registerproduct"><Nav.Link className="navbar-link">Register Product</Nav.Link></LinkContainer>
       </div>
       <div className="Navbar-logo">
         <h2>Decentralised Marketplace</h2>
